0798f80456208d2ac6ae2fa87b0a2f208e988cb2
maspypy <maspypy@gmail.com>
2020-02-22 00:04:43 +0900

    verify a+b

--- tests/aplusb.test.py
+++ tests/aplusb.test.py
@@ -1,6 +1,10 @@
 # verify-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
 # @import example_library/aplusb.py
 
+import sys
+sys.path.insert(0, '.')
+
+from example_library.aplusb import aplusb
 
 a, b = map(int, input().split())
 answer = a + b

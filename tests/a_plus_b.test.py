8e82980dfb9d89289be37e561e9d367438ef32b6
maspypy <maspypy@gmail.com>
2020-02-21 13:46:01 +0900

    fix

--- tests/a_plus_b.test.py
+++ tests/a_plus_b.test.py
@@ -1,8 +1,5 @@
 # verify-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
 # @import compro_library/example_library/a_plus_b.py
-import sys
-sys.path.insert(0, ".")
-
 from compro_library.example_library.a_plus_b import a_plus_b
 
 a, b = map(int, input().split())

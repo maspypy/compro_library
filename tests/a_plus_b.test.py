64cb8f1d1dee23dc76417d479e297589e8ed2481
maspypy <maspypy@gmail.com>
2020-02-21 23:33:28 +0900

    verify a+b

--- tests/a_plus_b.test.py
+++ tests/a_plus_b.test.py
@@ -1,5 +1,5 @@
 # verify-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
-# @import compro_library/example_library/a_plus_b.py
+# @import example_library/a_plus_b.py
 from example_library.a_plus_b import a_plus_b
 
 import sys

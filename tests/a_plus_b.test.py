fe0905a61944974c4b7a5cf423d6d353082b5120
maspypy <maspypy@gmail.com>
2020-02-21 23:28:40 +0900

    verify a+b

--- tests/a_plus_b.test.py
+++ tests/a_plus_b.test.py
@@ -2,6 +2,12 @@
 # @import compro_library/example_library/a_plus_b.py
 from compro_library.example_library.a_plus_b import a_plus_b
 
-a, b = map(int, input().split())
-answer = a_plus_b(a, b)
-print(answer)
+
+def main():
+    a, b = map(int, input().split())
+    answer = a_plus_b(a, b)
+    print(answer)
+
+
+if __name__ == '__main__':
+    main()

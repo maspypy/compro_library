a905611b4794b50829f27ff37399d464c9f26f29
maspypy <maspypy@gmail.com>
2020-02-23 01:32:46 +0900

    add euler_phi.test

---
+++ tests/aizu_online_judge/euler_phi.test.py
@@ -0,0 +1,23 @@
+# verify-helper: PROBLEM
+# @import number_theory/prime.py
+# @import number_theory/dirichlet_conbolution.py
+import sys
+sys.path.insert(0, '.')
+read = sys.stdin.buffer.read
+readline = sys.stdin.buffer.readline
+readlines = sys.stdin.buffer.readlines
+
+import numpy as np
+from number_theory.prime import prime_table
+from number_theory.dirichlet_convolution import euler_phi_table
+
+
+U = 10**6 + 1
+is_prime, primes = prime_table(U)
+phi = euler_phi_table(U, primes)
+A = phi.cumsum() + 1
+
+
+T = int(readline())
+N = np.array(read().split(), np.int32)
+print(*A[N], sep='\n')
